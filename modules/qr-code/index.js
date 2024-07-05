b0435bd86753c471fdcdd8f7634ea73dd8724db6
Hazen Wilson <hazenthewilson@gmail.com>
2024-07-05 00:12:15 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ modules/qr-code/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigator from "./Navigator";
+import { slice } from "./store";
+
+const QRCode = props => {
+  return <Navigator />;
+};
+
+export default {
+  title: "QRCode",
+  navigator: QRCode,
+  slice
+};
